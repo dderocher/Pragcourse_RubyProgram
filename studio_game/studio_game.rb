6791d7c27d676cacf3f6121d8093fb5ffbbcd892
dderocher <developer@defamily.org>
2014-07-09 17:26:45 -0400

    daily commit

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,8 +1,11 @@
-puts "Welcome!"
+name1 = 'larry'
+health1 = 60
 
-greeting = "Welcome!"
-3.times do
-  puts greeting.upcase
-end
- 
-puts Time.now
+puts " #{name1}'s health is #{health1 / 9.0}"
+
+
+player1 = 'larry'
+player2 = 'curly'
+player3 = 'moe'
+
+puts " Players:\n\t #{player1}\n\t #{player2}\n\t #{ player3}"
