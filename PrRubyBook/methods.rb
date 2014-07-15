c2154aae7bce5a0e754ee63ddcaa2529831d8d1d
dderocher <developer@defamily.org>
2014-07-15 17:00:52 -0400

    windows daily commit

--- PrRubyBook/methods.rb
+++ PrRubyBook/methods.rb
@@ -8,10 +8,94 @@ varargs("one")
 varargs("one",2)
 varargs("one",2,3)
 
-#Methods and Blocks
+puts <<-HEREDOC
+Methods and Blocks
+when a method is called
+it may be associated with a block. Normally, you call the block from within the method using
+yield: \n
+HEREDOC
+
+puts "\nMethod Block example 1 \n--------------------"
 def double(amt1)
   yield(amt1 * 2)
 end
 
 myvar = double(3) {|val| "I got #{val}"}
 puts myvar
+
+puts "\nMethod Block example 2 \n--------------------"
+puts <<-HEREDOC
+However, if the last parameter in a method definition is prefixed with an ampersand, any
+associated block is converted to a Proc object, and that object is assigned to the parameter.
+ This allows you to store the block for use later. \n
+HEREDOC
+
+
+class TaxCalculator
+  def initialize(name, &block)
+    @name, @block = name, block
+  end
+  def get_tax(amount)
+    "#@name on #{amount} = #{@block.call(amount) }"
+  end
+end
+
+tc = TaxCalculator.new("Dave Tax") {|amt| amt * 0.05}
+
+puts tc.get_tax(10) + "\n"
+
+
+puts "\nMethod Multiple Parameters example 1 \n--------------------"
+
+puts <<-HEREDOC_AGAIN
+As the last case illustrates, if you give return multiple parameters, the method returns them
+in an array. You can use parallel assignment to collect this return value
+HEREDOC_AGAIN
+
+def meth_three
+  100.times do |num|
+    square = num * num
+    return num, square if square > 1000
+  end
+end
+
+num, square = meth_three
+
+
+puts "\n Splat! Expanding Collections in Method Calls example 1 \n--------------------"
+
+puts <<-HEREDOC_AGAIN
+When you call a method, you can convert any collection or enumerable object into its con-­‐‑
+stituent elements and pass those elements as individual parameters to the method. Do this
+by prefixing array arguments with an asterisk:
+HEREDOC_AGAIN
+
+def five(a,b,c,d,e)
+  "I was passed #{a} #{b} #{c} #{d} #{e}"
+end
+
+puts "#{five(1,2,3,4,5)}"
+puts "#{five(1, 2, 3, *['a', 'b'])}"
+puts "#{five(*(10..14))}"
+
+puts "\n Making Blocks More Dynamic example 1 \n----------------------------------"
+
+puts <<-DAVE_COMMENT
+If the last argument to a method is preceded by an ampersand, Ruby assumes that it is a Proc
+object. It removes it from the parameter list, converts the Proc object into a block, and associates
+it with the method.
+DAVE_COMMENT
+
+print "(t)imes or (p)lus: "
+operator = gets
+print "number:  "
+number = Integer(gets)
+
+#lambda returns a proc object that contains the defined expression
+if operator = ~ /^t/
+  calc = lambda {|n| n * number }
+else
+  calc = lambda {|n| n * number }
+end
+
+puts ((1..10).collect(&calc).join(", ")   )
--- flicks/flicks.rb
+++ flicks/flicks.rb
@@ -1,3 +1,38 @@
+class Movie
+
+
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+    #puts "Creates a movie object with title #{title} and rank #{rank}"
+  end
+
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+  def to_s
+    "#{@title} has a rank of #{@rank}"
+  end
+end
+
+
+movie1 = Movie.new("goonies",10)
+movie1.thumbs_up
+puts movie1
+movie2 = Movie.new("ghostbusters",9)
+movie2.thumbs_down
+puts movie2
+movie3 = Movie.new("goldfinger")
+puts movie3
+
+
+
+
+=begin
 def weekday
   current_time = Time.new
   today = current_time.strftime("%A")
@@ -19,7 +54,7 @@ puts movie_listing("ghostbusters",7)
 a_title = 'goldfinger'
 puts movie_listing(a_title)
 
-
+=end
 
 
 
