5c19c2135898c01510dbd3891ce76ee8a2e32390
dderocher <developer@defamily.org>
2014-07-16 17:04:13 -0400

    windows daily commit

--- PrRubyBook/3.Classes-Objects-variables/books_in_stock.rb
+++ PrRubyBook/3.Classes-Objects-variables/books_in_stock.rb
@@ -15,18 +15,26 @@ class BookInStock
   end
 
   def price_in_cents
-     Integer(price*100 + .05)
+     Integer(price*100 + 0.5)
         # We multiply the floating-­‐‑point price by 100 to get the price in cents but then add 0.5 before converting
         # to an integer. Why? Because floating-­‐‑point numbers don’t always have an exact internal representation.
-        # When we multiply 33.8 by 100, we get 3379.99999999999954525265. The Integer method would truncate
-        # this to 3379. Adding 0.5 before calling Integer rounds up the floating-­‐‑point value, ensuring we get the
+        # When we multiply 33.8 by 100, we get 3379.. The Integer method would truncate
+        # this to 3379. Adding 0.5 before calling Int99999999999954525265eger rounds up the floating-­‐‑point value, ensuring we get the
         # best integer representation. This is a good example of why you want to use BigDecimal, not Float, in
         # financial calculations.
 
-   end
+  end
+
+  def price_in_cents=(cents)
+    @price =cents / 100.0
+  end
 end
 
 book = BookInStock.new("isbn1", 33.80)
 puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
+puts "Price in cents = #{book.price_in_cents}"
+
+book.price_in_cents = 1234
 puts "Price = #{book.price}"
 puts "Price in cents = #{book.price_in_cents}"
