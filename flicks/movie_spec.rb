4020e0d36cb29a7d3be3bf8b2983322f056ac573
dderocher <developer@defamily.org>
2014-10-29 17:22:29 -0400

    windows daily commit

--- flicks/movie_spec.rb
+++ flicks/movie_spec.rb
@@ -16,20 +16,51 @@ end
 
 describe Movie do
 
-  movie = Movie.new("goonies",10)
+
+  before do
+   @initial_rank = 10
+   @movie = Movie.new("goonies",10)
+ end
 
   it "has  capitalized title" do
-
+    movie = Movie.new("goonies",10)
     #  movie .title.should == "Goonies"
     #expect(movie.title).to eq("Goonies")
-    expect(movie.title)  ==  "Goonies"
+    expect(@movie.title).to  eq("Goonies")
   end
 
   it "has an initial rank" do
-    expect(movie.rank) == 10
+     expect(@movie.rank).to  eq(10)
   end
 
-  it 'has a string representation'
+  it 'has a string representation' do
+     expect(@movie.to_s).to eq('Goonies has a rank of 10')
+  end
+
+  it 'increased rank by 1 when given a thumbs up' do
+    @movie.thumbs_up
+
+    expect(@movie.rank).to eq(@initial_rank + 1)
+
+  end
+
+  it 'decreases  rank by 1 when given a thumbs down' do
+    @movie.thumbs_down
+    expect(@movie.rank).to eq(@initial_rank - 1)
+  end
+
+  context "created with a default rank" do
+    before do
+      @movie = Movie.new("goonies")
+    end
+
+    it "has a rank of 0" do
+      expect(@movie.rank).to  eq(0)
+    end
+
+  end
+
+
 end
 
 
